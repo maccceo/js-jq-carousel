7b2af4183c9a3bf0e020a0d92e7f9890c948bac1
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-21 17:13:37 +0200

    Anche i cerchi scorrono con le foto

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,16 +11,25 @@ function prevClick () {
 	var actualActive = $("img.active");
 	var lastItem = $("img.last");
 	var firstItem = $("img.first");
+	var circleActive = $("i.active");
+	var lastCircle = $("i.last");
+	var firstCircle = $("i.first");
 
 	// se torno indietro dalla prima devo attivare l'ultima
 	if (actualActive.hasClass("first")) {
 		firstItem.removeClass("active");
 		lastItem.addClass("active");
+		// cerchi
+		firstCircle.removeClass("active");
+		lastCircle.addClass("active");
 	}
 	// altrimenti attiva quella prima e disattivo quella attuale
 	else {
 		actualActive.prev().addClass("active");
 		actualActive.last().removeClass("active");
+		// cerchi
+		circleActive.prev().addClass("active");
+		circleActive.last().removeClass("active");
 	}
 };
 
@@ -28,15 +37,24 @@ function nextClick () {
 	var actualActive = $("img.active");
 	var lastItem = $("img.last");
 	var firstItem = $("img.first");
+	var circleActive = $("i.active");
+	var lastCircle = $("i.last");
+	var firstCircle = $("i.first");
 
 	// se clicco avanti dall'ultima devo tornare alla prima
 	if (actualActive.hasClass("last")) {
 		lastItem.removeClass("active");
 		firstItem.addClass("active");
+		// cerchi
+		lastCircle.removeClass("active");
+		firstCircle.addClass("active");
 	}
 	// altrimenti attiva quella dopo e disattivo quella attuale
 	else {
 		actualActive.next().addClass("active");
 		actualActive.first().removeClass("active");
+		// cerchi
+		circleActive.next().addClass("active");
+		circleActive.first().removeClass("active");
 	}
 };
